18f08ed95d3961e1b3351d3e9a4e4878f1644932
Damir Nasirov <krekki01@mail.ru>
2024-11-07 15:07:32 +0300

    feat: add base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import { AppHeader } from "components/AppHeader";
 import { ConstructorPage } from "pages/Constructor/ConstructorPage";
 import { loadIngredients } from "services/reducers/ingredientsSlice";
@@ -17,7 +18,16 @@ function App() {
   return (
     <main className={classes["main-wrapper"]}>
       <AppHeader />
-      <ConstructorPage />
+      <Routes>
+        <Route path="/" element={<ConstructorPage />} />
+        <Route path="/login" element={<div />} />
+        <Route path="/register" element={<div />} />
+        <Route path="/forgot-password" element={<div />} />
+        <Route path="/reset-password" element={<div />} />
+        <Route path="/profile" element={<div />} />
+        <Route path="/ingredients/:id" element={<div />} />
+        <Route path="*" element={<div />} />
+      </Routes>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { DndProvider } from "react-dnd";
@@ -10,10 +11,12 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
