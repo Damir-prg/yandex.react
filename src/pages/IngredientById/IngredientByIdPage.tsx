60d9fe8cb6908e3957b3839b71709e2ecd03d150
Damir Nasirov <krekki01@mail.ru>
2024-11-07 16:41:58 +0300

    feat: add ingredient by id page

--- src/pages/IngredientById/IngredientByIdPage.tsx
+++ src/pages/IngredientById/IngredientByIdPage.tsx
@@ -1,7 +1,32 @@
+import { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { IngredientDetails } from "components/IngredientsDetails";
+import { useAppSelector } from "services/hooks";
 import { PageContainer } from "../ui";
 
 import type { FC } from "react";
 
+import classes from "./ingredientByIdPage.module.css";
+
 export const IngredientByIdPage: FC = () => {
-  return <PageContainer>IngredientByIdPage</PageContainer>;
+  const { id } = useParams();
+  const { ingredients } = useAppSelector((state) => state.ingredients);
+
+  const selectedIngredient = useMemo(() => {
+    const foundedIngredient = ingredients.find(
+      (ingredient) => ingredient._id === id
+    );
+
+    return foundedIngredient;
+  }, [id, ingredients]);
+
+  return (
+    <PageContainer className={classes["ingredient-page"]}>
+      {selectedIngredient ? (
+        <IngredientDetails ingredient={selectedIngredient} />
+      ) : (
+        <h3 className="text text_type_main-medium">Ингредиент не найден</h3>
+      )}
+    </PageContainer>
+  );
 };
